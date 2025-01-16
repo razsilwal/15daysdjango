bbd722d5206450215df16c9f6adf6ea448e44960
razsilwal <sabalsilwal51@gmail.com>
2025-01-16 22:40:03 +0545

    day 7 digital pathsala

---
+++ blog/views/main_view.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request,'main/home.html')
+
+
+def single_blog(request):
+    return render(request,'main/single_blog.html')
+
+def edit_blog(request):
+    return render(request,'main/edit_blog.html')
+
+def create_blog(request):
+    return render(request,'main/create_blog.html')
